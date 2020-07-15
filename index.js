c060f667565ea4b857bc811ae1ebd1a58e4a60b4
ruchitp <ruchitpalrecha@hotmail.com>
2020-07-15 00:01:58 -0400

    Added heroku configs

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const express = require('express');
+const express = require('express')
+const path = require('path')
 const app = express()
 const port = 4000
 
@@ -6,4 +7,8 @@ app.get('/', (req, res) => {
     res.send('Hello World');
 })
 
-app.listen(port, () => console.log('Listening on port:', port))
+app
+    .use(express.static(path.join(__dirname, 'public')))
+    .set('views', path.join(__dirname, 'views'))
+    .set('view engine', 'ejs')
+    .listen(port, () => console.log('Listening on port:', port))
